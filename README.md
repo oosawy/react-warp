6294a73691040b32598e2e0794cd99ce18b70c1f
Yuta Osawa <yutaosawa81@gmail.com>
2023-02-12 11:13:29 +0900

    feat: prototype

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # react-warp
-make wormhole in react🐛
+
+Construct Warp Tunnels to warp elements between renders in React🪐
+
+> **Warning**
+> This library is still Proof of Concept, and not tested enough. Don't use this in production!
+
+## Motivation
+
+An article is work in progress.
+
+## Install
+
+```sh
+npm i @oosawy/react-warp
+```
+
+## Usage
+
+1. Add `<WarpProvider />` your app to prepare the warp tunnels.
+
+```tsx
+// if you use next.js, _app.tsx is good place to put WarpProvider
+import type { AppProps } from 'next/app'
+import { WarpProvider } from '@oosawy/react-warp'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WarpProvider>
+      <Component {...pageProps} />
+    </WarpProvider>
+  )
+}
+```
+
+2. Then wrap the element which you want to be warped with `<Warp />`. then the element will be warped to the same `<Warp />` in other renders.
+   Note: the children are only allowed to have one React element, which must have a `key` prop.
+
+```tsx
+const IndexPage = () => {
+  return (
+    <div>
+      <LandingLayout
+        header={
+          <Warp>
+            <input type="text" key="search-input" />
+          </Warp>
+        }
+      >
+        <h1>The search input warps when you navigate to another page!</h1>
+        <p>So the text you entered will persist even though its state is not managed.</p>
+      </LandingLayout>
+    </div>
+  )
+}
+
+const SearchPage = () => {
+  return (
+    <div>
+      <SearchLayout>
+        <Warp>
+          <input type="text" key="search-input" />
+        </Warp>
+      </SearchLayout>
+    </div>
+  )
+}
+```
---
+++ src/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useRef, useContext, useLayoutEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+type WarpManager = {
+  set(key: string, element: React.ReactElement): HTMLElement
+}
+
+const WarpContext = React.createContext<WarpManager | undefined>(undefined)
+
+type Portals = Record<
+  string,
+  { node: HTMLElement; element: React.ReactElement }
+>
+
+export const WarpProvider = (props: { children: React.ReactNode }) => {
+  const [portals, setPortals] = useState<Portals>({})
+
+  const manager: WarpManager = {
+    set(key, element) {
+      const node = portals[key]?.node ?? document.createElement('span')
+      setPortals((prev) =>
+        Object.assign({}, prev, { [key]: { node, element } })
+      )
+      return node
+    },
+  }
+
+  return (
+    <WarpContext.Provider value={manager}>
+      {props.children}
+      <WarpHost portals={portals} />
+    </WarpContext.Provider>
+  )
+}
+
+const WarpHost = (props: { portals: Portals }) => {
+  return (
+    <>
+      {Object.values(props.portals).map(({ node, element }) =>
+        ReactDOM.createPortal(element, node)
+      )}
+    </>
+  )
+}
+
+export const Warp = (props: { children: React.ReactElement }) => {
+  const manager = useContext(WarpContext)
+
+  if (!manager) {
+    throw new Error('<Warp /> must used under <WarpProvider />')
+  }
+
+  const ref = useRef<HTMLSpanElement>(null)
+
+  const key = React.Children.only(props.children).key
+
+  if (!key) {
+    throw new Error('<Warp /> child must have key prop')
+  }
+
+  useLayoutEffect(() => {
+    const node = manager.set(key.toString(), props.children)
+    ref.current?.replaceChildren(node)
+  }, [props.children, key])
+
+  return <span ref={ref} />
+}
